d58297eb2dfaea8045face04e4c82fac7d951670
CodeScott00 <dougal_00@hotmail.com>
2021-03-27 14:57:00 +0000

    MVP done

--- src/main/java/MusicShop.java
+++ src/main/java/MusicShop.java
@@ -1,14 +1,32 @@
+import behaviours.ISell;
+import instruments.Guitar;
+
+import java.util.ArrayList;
+
 public class MusicShop {
 
     private String name;
-    // arraylist?
+    private ArrayList<ISell> stock;
 
 
     public MusicShop(String name) {
         this.name = name;
+        this.stock = new ArrayList<>();
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getStockList() {
+        return this.stock.size();
+    }
+
+    public void addToStockList(ISell item){     //explain why isell is going in here?
+        stock.add(item);
+    }
+
+    public void removeItemFromStock(ISell item){
+        stock.remove(item);
     }
 }
--- src/test/java/MusicShopTest.java
+++ src/test/java/MusicShopTest.java
@@ -1,3 +1,5 @@
+import accessories.Sheetmusic;
+import instruments.Guitar;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -6,14 +8,45 @@ import static org.junit.Assert.assertEquals;
 public class MusicShopTest {
 
     MusicShop musicShop;
+    Guitar guitar;
+    Sheetmusic sheetmusic;
 
     @Before
     public void before() {
         musicShop = new MusicShop("Rays music shop");
+        guitar = new Guitar(20, 30, "wood", "blue", 6);
+        sheetmusic = new Sheetmusic(20,30,"galileo", "all the beats");
     }
 
     @Test
     public void hasName(){
         assertEquals("Rays music shop", musicShop.getName());
+    }
+
+    @Test
+    public void stockListStartsEmpty(){
+        assertEquals(0, musicShop.getStockList());
+    }
+
+    @Test
+    public void stockListCanAdd(){
+        musicShop.addToStockList(guitar);
+        assertEquals(1, musicShop.getStockList());
+    }
+
+    @Test
+    public void stockListCanAdd2(){
+        musicShop.addToStockList(guitar);
+        musicShop.addToStockList(sheetmusic);
+        assertEquals(2,musicShop.getStockList());
+    }
+                                                        // get this explained
+    @Test
+    public void canRemoveItemFromStock(){
+        musicShop.addToStockList(guitar);
+        musicShop.addToStockList(sheetmusic);
+        musicShop.removeItemFromStock(guitar);
+        assertEquals(1,musicShop.getStockList());
+
     }
 }
